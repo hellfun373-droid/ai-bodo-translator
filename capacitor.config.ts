f946f04af72a28b4c9fd9c90b7b16993f95a7de8
hellfun373-droid <hellfun373@gmail.com>
2025-10-08 05:48:08 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aibo.translator',
+  appName: 'AI Bodo Translator',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
